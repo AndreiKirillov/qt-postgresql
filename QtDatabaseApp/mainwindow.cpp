768ad6c756e9371619301394a99f00f81da7039e
AndreiKirillov <andreikiril1ov@yandex.ru>
2022-08-22 12:09:03 +0300

    connect to db

--- QtDatabaseApp/mainwindow.cpp
+++ QtDatabaseApp/mainwindow.cpp
@@ -3,7 +3,7 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow), db(), is_connected(false)
 {
     ui->setupUi(this);
 }
@@ -13,3 +13,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// Кнопка подключения к базе данных
+void MainWindow::on_connect_btn_clicked()
+{
+    if(!is_connected)
+    {
+        db = QSqlDatabase::addDatabase("QPSQL");
+        db.setHostName("127.0.0.1");
+        db.setDatabaseName("testdb");
+        db.setUserName("myuser");
+        db.setPassword("123");
+        if(db.open())
+        {
+            is_connected = true;
+            QMessageBox::about(this, "Внимание", "Подключение к базе удалось!");
+        }
+        else
+            QMessageBox::about(this, "Внимание", "Не удалось подключиться!");
+    }
+}
--- QtDatabaseApp/mainwindow.h
+++ QtDatabaseApp/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMessageBox>
+#include <QSqlDatabase>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_connect_btn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSqlDatabase db;
+    bool is_connected;
 };
 #endif // MAINWINDOW_H
